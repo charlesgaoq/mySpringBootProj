25697a61d4a1020e6276603b147ad17669db85b5
charlesgaoq <478484836@qq.com>
2020-07-19 15:31:59 +0800

    export-master

---
+++ export-master/src/main/java/com/wangchi/demo/export/Test.java
@@ -0,0 +1,28 @@
+package com.wangchi.demo.export;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class Test {
+
+    public static void main(String[] args) {
+//        List<String> list = new ArrayList<String>();
+//        list.add("hubei");
+//        list.add("beijing");
+//        list.add("shanghai");
+//        System.out.println(list);
+//        list.set(0, "guanghsui");
+//        list.remove(list.size() - 1);
+//        System.out.println(list);
+        Integer x = 3;
+        if (3 == x) {
+            System.out.println("666");
+        }
+    }
+
+}
